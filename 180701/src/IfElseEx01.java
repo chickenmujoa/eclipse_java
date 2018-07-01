fdf1748fb4ff8ff6b06218f6e6edbdde32e8a196
Matthew Song <chickenmujoa.gmail.com>
2018-07-01 21:12:29 +0900

    IfElseEx01.java modified on Local Repository. Added else-if

--- 180701/src/IfElseEx01.java
+++ 180701/src/IfElseEx01.java
@@ -4,15 +4,24 @@ public class IfElseEx01 {
 		printAge(20);
 		printAge(15);
 		printAge(10);
+		printAge(5);
 	}
 	
 	public static void printAge(int age) {
-		if (age > 18) {
+		if (age > 18) {								// 18세 이상인 경우
 			System.out.println("성인입니다");
 		}
 		
+		else if ((18 >= age) && (age >= 13)) {		// 13~18세 인 경우
+			System.out.println("청소년입니다");
+		}
+		
+		else if ((13 > age) && (age >= 6)) {		// 6~12세 인 경우
+			System.out.println("어린이입니다");
+		}
+		
 		else {
-			System.out.println("미성년자입니다");
+			System.out.println("유아입니다");
 		}
 	}
 }
