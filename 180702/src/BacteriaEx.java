2bfc8b4e11577cf35597aef274ba72cb7b50a973
Matthew Song <chickenmujoa@gmail.com>
2018-07-02 13:27:41 +0900

    BacteriaEx.java made in Local Repository

---
+++ 180702/src/BacteriaEx.java
@@ -0,0 +1,19 @@
+/**
+ * 하루에 두 배씩 번식하는 세균
+ * 1, 2, 4, 8, 16, 32, 64, 128, ...
+ * http://cloudstudying.kr/lectures/193
+ */
+
+import java.util.Arrays;
+
+public class BacteriaEx {
+	public static void main(String[] args) {
+		//배열
+		int values[] = new int[10];
+		
+		// 박테리아의 개체수 변화를 배열에 저장하시오
+		
+		// 출력
+		System.out.println(Arrays.toString(values));
+	}
+}
